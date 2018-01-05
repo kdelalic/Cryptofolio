abd056983c0887afb4beb8b5ecc944dd5cb75eb8
delalik <delalik@mcmaster.ca>
2018-01-05 12:48:34 -0500

    initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/app.js';
+import registerServiceWorker from './js/registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './css/app.css';
+
+class App extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default App;
